efd32569d481cb082123d16fec62f7a097ab26c4
Felipe Marques <felipe.tecnosuporte@gmail.com>
2021-04-12 02:06:15 -0300

    estruturando o checkout

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,22 +1,27 @@
 import styles from './styles.module.scss';
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/client';
 
 export function SingInButton(){
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session] = useSession();
+    //console.log('session', session);
+    // se tiver session 
+    return session ? (
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={() => signOut()}
             >
             <FaGithub color="#04d361"/>
-          Felipe Marques
+         {session.user.name}
           <FiX color="#737380" className={styles.closeIcon}/>
         </button>  
     ) : (
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={() => signIn('github')}
             >
             <FaGithub color="#eba417"/>
           Sing In whith Github
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,6 +7,17 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId} : SubscribeButtonProps){
+    const [session] = useSession();
+    function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        // criação da checkout session
+        
+
+    }
     return (
         <button
             type="button"
