eb91ffba9c0fd01fe81b15dc657cc771d6b60e3e
Felipe Marques <felipe.tecnosuporte@gmail.com>
2021-07-03 09:35:41 -0300

    teste variable

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        STRIPE_API_KEY: 'mysk_test_51IejCpABqF8LxqJ86eUw2L3c7zbdiKPrM6Oc5ny4tZgjScdbQcZUpYQQuaIYScUZrM5an74iUScqAKwvo3X1KAS300tZrBLjJB-value',
+        STRIPE_WEBHOOK_SECRET: 'whsec_na4RnBreaTvtqXxMuYhU9NKodNVR5DqQ',
+        STRIPE_SUCCESS_URL: "http://localhost:3000/posts",
+        STRIPE_CANCEL_URL: 'http://localhost:3000/',
+        NEXT_PUBLIC_STRIPE_PUBLIC_KEY: 'pk_test_51IejCpABqF8LxqJ8aow3DBjLjEQeSQLMX7tCUw8CzOCzdbxYb76mFfIDzpGXG5BnRI0H8oymTlDmm7tLiZwqQSg900g2hQ7tZ4',
+        GITHUB_CLIENT_ID: '218e07b5878a69269e3d',
+        GITHUB_CLIENT_SECRET: '998c92fb4b7a86371b1201df9675dc22b703f3f6',
+        FAUNADB_KEY: 'fnAEGk0lxsACBMwPVTXk_gW5U9LlQarG4rXQZccr',
+        PRISMIC_ENDPOINT ='https://ignews2301.cdn.prismic.io/api/v2',
+        PRISMIC_ACCESS_TOKEN: 'MC5ZSWlyTnhJQUFDTUF5VDlS.D--_ve-_vVHvv70D77-9fO-_ve-_vTjvv70wF2Y3TCjvv71F77-977-977-977-9PGrvv71KUe-_ve-_ve-_vQ',
+
+    },
+  }
